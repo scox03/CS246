9c721914a0163b2e09b267c792537278e3753e36
Shania Cox <67437724+scox03@users.noreply.github.com>
2020-08-27 20:11:49 -0400

    Add files via upload
    This is my in class lab work. I'm not sure if it is showing up properly on Dropbox or not so this is my backup method.

--- Labs/lab1A.cpp
+++ Labs/lab1A.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
 #include <string>
 
-int main()
-{
-	return 0;
+using namespace std; 
+
+bool HasDuplicates(double arr[], int size) {
+  for(int i = 0; i < size - 1; i++) {
+    for(int j = i + 1; j < size; j++){
+      if(arr[i] == arr[j]) {
+        return true;
+      } else {
+        return false;
+      }
+    } 
+  }
+  return 0;
 }
+
+int main() {
+  cout << HasDuplicates(numbers, 4);
+  return 0;
+}
+
+
+void TrimEnds(string word) {
+  string str = word;
+  int space = 0;
+  for(int i = 0; i < str.length() - 1; i++) {
+    if(str[i] == ' ') {
+      str[i] = str[space++];
+    }
+    space++;
+  }
+  cout << str;
+}
+
+int main() 
+{ 
+    string str = " hello world "; 
+    TrimEnds(str);  
+    return 0; 
+} 
